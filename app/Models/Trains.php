02549f31d7cad5f852cef9f2755422545c2a1188
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-01 16:18:17 +0100

    Creata tabella su my sql

--- app/Models/Trains.php
+++ app/Models/Trains.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Trains extends Model
 {
     use HasFactory;
+    private $id;
+    private $place_of_departure;
+    private $departure_time;
+    private $place_of_arrival;
+    private $arrival_time;
+    private $price;
+    private $train_code;
+    private $owner_company;
+
+ 
 }
---
+++ database/migrations/2023_02_01_115929_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('place_of_departure', 64);
+            $table->dateTime('departure_time');
+            $table->string('place_of_arrival');
+            $table->dateTime('arrival_time');
+            $table->integer('price')->unsigned();
+            $table->string('train_code', 32);
+            $table->string('owner_company', 64);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
