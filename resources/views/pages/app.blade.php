f8686d88405d68a0f4940c6f81e873d0d397dcb1
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-01 17:09:48 +0100

    Collegamento tabella alla pagina

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -1,12 +1,63 @@
 @extends('layout.main-layout')
 
 @section('list-train')
-    <ul>
-        @foreach ($trains as $train)
-            <li>
-                {{$train -> place_of_departure}}
-            </li>
-        @endforeach
-    </ul>
+<table>
+    <tr>
+      <th> place_of_departure</th>
+      <th> departure_time</th>
+      <th> departure_date</th>
+      <th> place_of_arrival</th>
+      <th> arrival_time</th>
+      <th> arrival_date</th>
+      <th> price</th>
+      <th> train_code</th>
+      <th> owner_company</th>
+    </tr>
+    @foreach ($trains as $train)
+        <tr>
+            <td>
+            
+                {{$train ->  place_of_departure}}
+            </td>
+            <td>
+            
+                {{$train ->  departure_time}}
+            </td>
+            <td>
+            
+                {{$train ->  departure_date}}
+            </td>
+            <td>
+            
+                {{$train ->  place_of_arrival}}
+            </td>
+            <td>
+            
+                {{$train ->  arrival_time}}
+            </td>
+            <td>
+            
+                {{$train ->  arrival_date}}
+            </td>
+            <td>
+            
+                {{$train ->  price}}
+            </td>
+            <td>
+            
+                {{$train ->  train_code}}
+            </td>
+            <td>
+            
+                {{$train ->  owner_company}}
+            </td>
+        </tr>
+        
+
+    @endforeach
+
+
+  </table>
+
 
 @endsection
